b9438a53266dffd39b06928deec61f14fe26f622
ashikurrafi <ashikurrafi@gmail.com>
2021-10-23 01:12:17 +0600

    Add project files.

---
+++ Progress bar/Progress bar.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+#include<string>
+#include<thread>
+void show_progress_bar(std::ostream& os, int time, std::string message, char symbol = '*')
+{
+	static const auto bar_length = 101;
+	if (message.length() >= bar_length)
+	{
+		os << message << '\n';
+		message.clear();
+	}
+	else
+	{
+		message += " ";
+	}
+	const auto progress_level = 101.0 / (bar_length - message.length());
+	std::cout << message;
+	for (double percentage = 0; percentage <= 101; percentage += progress_level)
+	{
+		message += symbol;
+		os << "\r[" << std::setw(3) << static_cast<int>(percentage) << "%]" << message << std::flush;
+		std::this_thread::sleep_for(std::chrono::milliseconds(time));
+	}
+	os << "\n\n";
+}
+int main()
+{
+	show_progress_bar(std::clog, 101, " Progress : ", '#');
+	return 0;
+}
